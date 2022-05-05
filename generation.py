58f9b1d3ca0744f4eb6b0ec1c8da7b8b47d3f195
Mikhail <mitchell1@mail.ru>
2022-05-05 22:26:12 +0300

    users.py Добавлена функция авторизации пользователя

---
+++ generation.py
@@ -0,0 +1,9 @@
+"""
+Генерация данных (категории, товары, пользователи)
+"""
+import random
+
+login = ['IvanovAA','PetrovAS']
+password_char = '1234567890'
+
+print(type(login))
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import users
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print('Добро пожаловать в интернет-магазин продуктов питания!')
+print('Просим Вас авторизоваться')
+login=input('Введите Ваш Логин:')
+password=input('Введите Ваш Пароль:')
+users.user_authorization(login,password)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
