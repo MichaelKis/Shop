e8c2ef990d955748d9b3e428987afbfc50e797c7
Миша <mitchell1@mail.ru>
2022-05-06 17:08:00 +0300

    Чтение БД

--- generation.py
+++ generation.py
@@ -1,9 +1,32 @@
 """
 Генерация данных (категории, товары, пользователи)
 """
-import random
+import dbf
 
-login = ['IvanovAA','PetrovAS']
-password_char = '1234567890'
+db = dbf.Table('products.dbf')
+db.open()
+print(db)
+# db = dbf.Dbf('products.dbf')
 
-print(type(login))
+products_list = ['AAA', 'BBB', 'CCC', 'DDD']
+
+SCHEMA = (
+    ("ID", "N", 8),
+    ("PRODUCT", "C", 200),
+    ("PRIСE", "N", 8, 2),
+    ("DATE", "D"),
+)
+
+db = dbf.Dbf("products.dbf", new=True)
+db.addField(*SCHEMA)
+
+id_ = 0
+for product in products_list:
+    rec_ = db.newRecord()
+    rec_["ID"] = product.id_
+    rec_["PRODUCT"] = product.product.encode("cp866")
+    rec_["PRICE"] = product.price
+    rec_["DATE"] = product.date
+    id_ += 1
+    rec_.store()
+db.close()
