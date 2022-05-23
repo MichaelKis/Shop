b6066dec6c322c80c7bb950e4ad4737ff90b3e8a
Mikhail Kiselev <mitchell1@mail.ru>
2022-05-23 18:31:00 +0300

    mony

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from goods import Goods
 
 # Регистрация пользователя
 
-# print('Добро пожаловать в интернет-магазин продуктов питания!')
-# print('Просим Вас авторизоваться')
-# login=input('Введите Ваш Логин:')
-# password=input('Введите Ваш Пароль:')
-#
-# user_authorization(login,password)
+print('Добро пожаловать в интернет-магазин продуктов питания!')
+print('Просим Вас авторизоваться')
+login=input('Введите Ваш Логин:')
+password=input('Введите Ваш Пароль:')
+
+user_authorization(login,password)
 
 # Создание товаров
 
